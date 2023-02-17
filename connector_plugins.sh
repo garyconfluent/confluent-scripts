e2b68d505477e2a2740a7946d4d3ef5202a55cb7
root <root@MJ0G9FV0>
2023-02-17 10:35:27 -0500

    adding Connector_plugin script

---
+++ connector_plugins.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+start=`date +%s`
+
+# Array of Connector Names (strings) that you care about
+
+connectors=(
+   "kafka-connect-ibmmq"
+   "snowflake-kafka-connector"
+   "kafka-connect-replicator"
+   "kafka-connect-servicenow"
+   "kafka-connect-json-schema"
+   "kafka-connect-salesforce"
+   "kafka-connect-s3-source"
+)
+# output format that jq will produce (each entry on seperate line)
+output_format=".name + \":\" + .version + \" - Release Date: \" + .release_date"
+
+# curl the manis and then sort then by release_date desc and save it to allmanis.json
+curl -s -S 'https://api.hub.confluent.io/api/plugins?per_page=100000' | jq '. | sort_by(.release_date) | reverse | .' > allmanis.json
+
+# Hack to create a comma-seperated, double quote escaped string from the array to feed jq 'IN'
+connectors_string=""
+delim=""
+for conn in "${connectors[@]}"; do
+  connectors_string="$connectors_string$delim\"$conn\""
+  delim=","
+done
+
+# filter allmanis.json for just these connectors (maintaining the sort of release_date desc)
+echo "Latest Connector Versions as of $(date '+%F %T')"
+echo "#####################################################"  
+jq '.[] | select(IN(.name; '"${connectors_string}"')) | '"${output_format}"'' allmanis.json
+
+# rm allmanis.json
+
+end=`date +%s`
+duration=$((end-start))
